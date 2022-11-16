08ffddb4d28f5214d7ba774cd5335020d2e646b6
lstnchDev <freest2334@gmail.com>
2022-11-16 21:38:57 +0300

    fix error auth

--- src/components/auth/Oauth2.jsx
+++ src/components/auth/Oauth2.jsx
@@ -2,17 +2,20 @@ import OAuth2Login from 'react-simple-oauth2-login';
 import { useAppDispatch } from '../../redux/store/store';
 import { fetchLogin } from "../../redux/slices/loginStateSlices"
 import styles from "./css/oauth2.module.scss"
-import { auth, authorizationUrl, clientId, redirectUri, scope, state } from '../../consts/variableConsts';
 
+const clientId = "0zuarw2s00p8z3hy0kxcr3q5ufc7gm"
+const redirectUri = "http://localhost:3000"
+const authorizationUrl = "https://id.twitch.tv/oauth2/authorize"
+
+const scope = "channel%3Amanage%3Apolls+channel%3Aread%3Apolls+user:read:follows"
+const state = "c3ab8aa609ea11e793ae92361f002671"
 
 const Oauth2 = ()=>{
     const dispatch = useAppDispatch()
     const onSuccess = response => {
       const dateCookie = new Date(Date.now())
-      console.log(dateCookie)
-
       dateCookie.setDate(dateCookie.getDate() + 6)
-        setCookie(auth, response.access_token, {
+        setCookie('auth', response.access_token, {
           expires: dateCookie
         })
         dispatch(fetchLogin(response.access_token))
@@ -21,9 +24,8 @@ const Oauth2 = ()=>{
     const onFailure = response => console.log(response);
     
     const setCookie = (name, value, options = {})=>{
-      console.log(123)
         options = {
-            path: 'https://lstnchdev.github.io/twitch-follow/',
+            path: 'http://localhost:3000',
             ...options
           };
           if (options.expires instanceof Date) {
@@ -49,14 +51,13 @@ const Oauth2 = ()=>{
             id="auth-code-login-btn"
             authorizationUrl={authorizationUrl}
             clientId={clientId}
-            redirectUri='localhost:3000/twitch-follow/'
+            redirectUri={redirectUri}
             responseType="token"
             scope={scope}
             state={state}
             buttonText="Login"
             className={styles.authBtn}
             onSuccess={onSuccess}
-            onRequest={onFailure}
             onFailure={onFailure}
       />
     )
